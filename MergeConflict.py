f2fb4c7f4998e4878d030c886ec0e602e4a84702
teejay95 <teejay95@vt.edu>
2018-09-26 19:46:45 -0400

    Whatevs

--- MergeConflict.py
+++ MergeConflict.py
@@ -4,8 +4,5 @@
 # Each person should change to following line of code:
 
 print("This was initially just a simple sentence, but now I made it look like code. It doesn't actually matter, but it'll look more familiar this way");
-<<<<<<< HEAD
+
 print("Hello world, my name is Tyler and my favorite programming language is Java ");
-=======
-print("Hello world, my name is DAVID and my favorite programming language is PYTHON ");
->>>>>>> ae335be81f1a245a0dc4151852e0c0e867793c5e
